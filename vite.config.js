0859ae5c3f08857634d62788e6fd3ea9e591725f
mdfakih <mf9049@gmail.com>
2025-04-05 10:05:31 +0530

    new push

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  base: '/USERCRUD/', // <-- 👈 add this line (use your repo name)
+  base: '/USERCRUD/',
   plugins: [react()],
 });
